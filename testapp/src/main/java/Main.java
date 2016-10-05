eefb40c548148c7fb84ccce9aa25d55eab8c2327
Radim <strejcek11@seznam.cz>
2016-10-05 16:55:42 +0200

    commit

--- testapp/src/main/java/Main.java
+++ testapp/src/main/java/Main.java
@@ -3,14 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package cz.vsb.p4.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
- *
- * @author str0238
+ * @author ruz76
  */
-public class Main {
-    public static void main(String[]args){
-        System.out.println();
-        
+@SpringBootApplication(scanBasePackages = {"cz.vsb"})
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Main.class);
     }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
 }
